0bd45bfadf040b23c0c25f03c39558983468db7d
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2023-09-01 22:11:41 +0400

    commit

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <thread>
 #include <iostream>
 
-void print_numbers_in_background() {
+void print_numbers_in_background(int end) {
     auto ix{0};
 // Attention: an infinite loop!
-    while (true) {
+    while (ix <= end) {
         std::cout << "Background: " << ix++ << std::endl;
     }
 }
 
 int main() {
-    std::thread background{print_numbers_in_background};
+    std::thread background{print_numbers_in_background, 10};
     auto jx{0};
     while (jx < 100) {
         std::cout << "Main: " << jx++ << std::endl;
     }
+    // background.join(); for waiting thread
 }
