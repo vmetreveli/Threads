62afdf9b8f5a2099ffacf26e1df8497b37a0c080
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2023-09-05 15:04:04 +0400

    commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
+//#include <thread>
+//#include <iostream>
+//
+//void print_numbers_in_background(int end) {
+//    auto ix{0};
+//// Attention: an infinite loop!
+//    while (ix <= end) {
+//        std::cout << "Background: " << ix++ << std::endl;
+//    }
+//}
+//
+//int main() {
+//    std::thread background{print_numbers_in_background, 10};
+//    auto jx{0};
+//    //auto jx=0;
+//
+//    while (jx < 100) {
+//        std::cout << "Main: " << jx++ << std::endl;
+//    }
+//    // background.join(); for waiting thread
+//}
+
+//
 #include <thread>
 #include <iostream>
-
-void print_numbers_in_background(int end) {
-    auto ix{0};
-// Attention: an infinite loop!
-    while (ix <= end) {
-        std::cout << "Background: " << ix++ << std::endl;
-    }
+std::atomic_int a=0;
+int counter = 0;
+std::mutex m;
+void foo() {
+    std::lock_guard g{m};
+    counter++;
 }
 
 int main() {
-    std::thread background{print_numbers_in_background, 10};
-    auto jx{0};
-    while (jx < 100) {
-        std::cout << "Main: " << jx++ << std::endl;
-    }
-    // background.join(); for waiting thread
+    std::thread A{foo};
+    std::thread B{foo};
+    std::thread C{[] { foo(); }};
+    std::thread D{[] {
+        for (int i = 0; i < 10; ++i) {
+            foo();
+        }
+    }};
+    std::cout << "Count: " << counter;
 }
